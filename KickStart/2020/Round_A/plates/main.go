b490b20f7a92ccf47ea37f0e5eb880b9b1b111fc
Mohammad Ishfaque Jahan Rafee <3380056+rafee@users.noreply.github.com>
2020-04-16 17:11:12 -0400

    Updated with fmt.Fscan

--- KickStart/2020/Round_A/plates/main.go
+++ KickStart/2020/Round_A/plates/main.go
@@ -2,83 +2,70 @@ package main
 
 import (
 	"bufio"
+	"container/heap"
 	"fmt"
-	"io"
 	"os"
-	"strconv"
 )
 
 func main() {
-	scanner := getScanner(os.Stdin)
-	// Sets words as the split function. i.e. separated by space
-	scanner.Split(bufio.ScanWords)
-	N, _ := readInt(scanner)
-	for i, house, budget := 1, 0, 0; i <= N; i++ {
-		house, _ = readInt(scanner)
-		budget, _ = readInt(scanner)
-		prices := make([]int, 1000)
-		for j, val := 0, 0; j < house; j++ {
-			val, _ = readInt(scanner)
-			prices[val-1]++
-		}
-
-		count := 0
-		for j := 0; j < 1000; j++ {
-			if prices[j] == 0 {
-				continue
+	in := bufio.NewReader(os.Stdin)
+	var T int
+	fmt.Fscan(in, &T)
+	for i := 1; i <= T; i++ {
+		var N, K, P int
+		fmt.Fscan(in, &N, &K, &P)
+		plates := make([][]int, N)
+		h := &PlateHeap{}
+		heap.Init(h)
+		for n := 0; n < N; n++ {
+			plates[n] = make([]int, K)
+			for k := 0; k < K; k++ {
+				fmt.Fscan(in, &plates[n][k])
 			}
-			cost := prices[j] * (j + 1)
-			if budget >= cost {
-				budget -= cost
-				count += prices[j]
-			} else {
-				c := budget / (j + 1)
-				count += c
-				break
+			heap.Push(h, [3]int{plates[n][0], n, 0})
+		}
+		beauty := 0
+		for p := 0; p < P; p++ {
+			plate := heap.Pop(h).([3]int)
+			beauty += plate[0]
+			n, k := plate[1], plate[2]
+			if k != (K - 1) {
+				heap.Push(h, [3]int{plates[n][k+1], n, k + 1})
 			}
 		}
-		fmt.Printf("Case #%d: %d\n", i, count)
+		fmt.Printf("Case #%d: %d\n", i, beauty)
 	}
 }
 
-// getScanner returns a scanner buffer from input file/io e.g. os.StdIn
-func getScanner(input *os.File) *bufio.Scanner {
-	in := bufio.NewReader(input)
-	scanner := bufio.NewScanner(in)
-	return scanner
+// An PlateHeap is a max-heap of ints.
+type PlateHeap [][3]int
+
+func (h PlateHeap) Len() int           { return len(h) }
+func (h PlateHeap) Less(i, j int) bool { return h[i][0] > h[j][0] }
+func (h PlateHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *PlateHeap) Push(x interface{}) {
+	// Push and Pop use pointer receivers because they modify the slice's length,
+	// not just its contents.
+	*h = append(*h, x.([3]int))
 }
 
-// readInt returns the next integer from with scanner input. It also returns any
-// error encountered
-func readInt(scanner *bufio.Scanner) (int, error) {
-	// scan for the next token
-	if !scanner.Scan() {
-		return 0, io.ErrUnexpectedEOF
-	}
-	// manually parse existing bytes without allocation
-	i, err := atoi(scanner.Bytes())
-	return i, err
+func (h *PlateHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
 }
 
-// atoi parses a buffer into an integer
-// without overflow detection.
-func atoi(b []byte) (int, error) {
-	neg := false
-	if b[0] == '+' {
-		b = b[1:]
-	} else if b[0] == '-' {
-		neg = true
-		b = b[1:]
-	}
-	n := 0
-	for _, v := range b {
-		if v < '0' || v > '9' {
-			return 0, strconv.ErrSyntax
-		}
-		n = n*10 + int(v-'0')
-	}
-	if neg {
-		return -n, nil
-	}
-	return n, nil
-}
+// // This example inserts several ints into an IntHeap, checks the minimum,
+// // and removes them in order of priority.
+// func main() {
+// 	h := &PlateHeap{2, 1, 5}
+// 	heap.Init(h)
+// 	heap.Push(h, 3)
+// 	fmt.Printf("minimum: %d\n", (*h)[0])
+// 	for h.Len() > 0 {
+// 		fmt.Printf("%d ", heap.Pop(h))
+// 	}
+// }
